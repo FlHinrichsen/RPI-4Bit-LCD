90f1c54e05a045b3422b348d5215d767962f4c9b
Florian Hinrichsen <florian.hinrichsen@werum.com>
2024-04-18 09:12:07 +0200

    #11: Add display mode and options

--- RPI-4Bit-LCD/LcdDriver.cs
+++ RPI-4Bit-LCD/LcdDriver.cs
@@ -42,6 +42,9 @@ namespace RPI_4Bit_LCD
       this.lcdWidth = lcdWidth;
     }
 
+    /// <summary>
+    /// Initializes the display
+    /// </summary>
     public void Init()
     {
       Pi.Gpio[lcdRs].PinMode = GpioPinDriveMode.Output;
@@ -54,16 +57,74 @@ namespace RPI_4Bit_LCD
       lcd_send_byte(0x33, lcdCmd);
       lcd_send_byte(0x32, lcdCmd);
       lcd_send_byte(0x28, lcdCmd);
-      lcd_send_byte(0x0C, lcdCmd);
-      lcd_send_byte(0x06, lcdCmd);
+      DisplayOptions(true,false,false);
+      DisplayMode(true,false);
       Clear();
     }
 
+    /// <summary>
+    /// Sets the display options
+    /// </summary>
+    /// <param name="d">true for display on, false for off</param>
+    /// <param name="c">true to show the curser, false for hide</param>
+    /// <param name="b">true for flash the curser, false for static</param>
+    public void DisplayOptions(bool d, bool c, bool b)
+    {
+      byte data = 0x08;
+
+      if (d)
+      {
+        data = (byte)(data & 0x04);
+      }
+
+      if (c)
+      {
+        data = (byte)(data & 0x02);
+      }
+
+      if (b)
+      {
+        data = (byte)(data & 0x01);
+      }
+
+      lcd_send_byte(data, lcdCmd);
+    }
+
+    /// <summary>
+    /// Sets the display mode
+    /// </summary>
+    /// <param name="id">true for curser increment, false for decrement</param>
+    /// <param name="s">true for shifting the text, false for fix text position</param>
+    public void DisplayMode(bool id, bool s)
+    {
+      byte data = 0x04;
+
+      if (id)
+      {
+        data = (byte)(data & 0x02);
+      }
+
+      if (s)
+      {
+        data = (byte)(data & 0x01);
+      }
+
+      lcd_send_byte(data, lcdCmd);
+    }
+
+    /// <summary>
+    /// Clears the screen
+    /// </summary>
     public void Clear()
     {
       lcd_send_byte(0x01, lcdCmd);
     }
 
+    /// <summary>
+    /// Clears the screen and write both lines
+    /// </summary>
+    /// <param name="l1">Text for line 1</param>
+    /// <param name="l2">Text fpr line 2</param>
     public void WriteScreen(string l1, string l2)
     {
       Clear();
@@ -71,11 +132,19 @@ namespace RPI_4Bit_LCD
       WriteLine2(l2);
     }
 
+    /// <summary>
+    /// Writes line 1 without clear
+    /// </summary>
+    /// <param name="text">text to write</param>
     public void WriteLine1(string text)
     {
       WriteLine(text, lcdLine1);
     }
 
+    /// <summary>
+    /// Writes line 2 without clear
+    /// </summary>
+    /// <param name="text">text to write</param>
     public void WriteLine2(string text)
     {
       WriteLine(text, lcdLine2);
